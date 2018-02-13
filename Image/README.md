f86ecf65ac8e9a964184258be32178ecef1524d0
montimaj <monti.majumdar@gmail.com>
2018-02-13 22:56:13 +0530

    Added 16-bit image support using GDAL

---
+++ Image/README.md
@@ -0,0 +1,14 @@
+# RGB, NIR to NDVI
+
+## IKONOS RGB
+![Alt text](./Data/ikonos-rgb.tif?raw=true "RGB")
+
+## IKONOS NIR
+![Alt text](./Data/ikonos-nir.tif?raw=true "NIR")
+
+## GENERATED NDVI 8 bit
+![Alt text](./Outputs/ndvi_8.tif?raw=true "NDVI8")
+
+## GENERATED NDVI 8 bit
+![Alt text](./Outputs/ndvi_16.tif?raw=true "NDVI16")
+
--- Image/ndvi.py
+++ Image/ndvi.py
@@ -1,9 +1,13 @@
 from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
-import math
+import numpy as np
+from osgeo import gdal
 
-def read_image(img):
-    img = Image.open(img)
+def read_image(img, bit=8):
+    if bit<=8:
+        img = Image.open(img)
+    else:
+        img = gdal.Open(img)
     return img
 
 def show_histogram(img, fig='1', bandwise=True):
@@ -39,10 +43,11 @@ def draw_lines(img):
     draw.line((0, img.size[1], img.size[0], 0), fill=128)
     return img
 
-def scale_ndvi(ndvi):
+def scale_ndvi(ndvi, bit=8):
+    scale_factor=2**(bit - 1)
     if ndvi<=0:
-        return round(math.fabs(ndvi*128))
-    return round(ndvi*128+127)
+        return int(round(np.abs(ndvi*scale_factor)))
+    return round(ndvi*scale_factor+scale_factor-1)
 
 def generate_ndvi_image(rgbimg, nirimg):
     rimg=rgbimg.split()[0]
@@ -52,14 +57,44 @@ def generate_ndvi_image(rgbimg, nirimg):
     for i in range(rows):
         for j in range(cols):
             pos=(j,i)
-            r=rimg.getpixel(pos)
-            n=nimg.getpixel(pos)
-            ndvi=0
-            if n+r!=0:
-                ndvi=(n-r)/(n+r)
-            ndvi_img.putpixel(pos,(scale_ndvi(ndvi),))
+            r=float(rimg.getpixel(pos))
+            n=float(nimg.getpixel(pos))
+            ndvi=0.
+            d=n+r
+            if d != 0:
+                num=n-r
+                ndvi=num/d
+            ndvi_img.putpixel(pos,(scale_ndvi(ndvi, 8),))
     return ndvi_img
 
+def save_img_16(input_img, arr_out, outfile):
+    cols,rows=input_img.shape
+    driver = gdal.GetDriverByName("GTiff")
+    outdata = driver.Create(outfile, rows, cols, 1, gdal.GDT_UInt16)
+    #outdata.SetGeoTransform(input_img.GetGeoTransform())  ##sets same geotransform as input
+    #outdata.SetProjection(input_img.GetProjection())  ##sets same projection as input
+    outdata.GetRasterBand(1).WriteArray(arr_out)
+    #outdata.GetRasterBand(1).SetNoDataValue(10000)  ##if you want these values transparent
+    outdata.FlushCache()
+    
+def generate_ndvi_image_16(rgbimg, nirimg):
+    rimg=rgbimg.GetRasterBand(1).ReadAsArray()
+    nimg=nirimg.GetRasterBand(1).ReadAsArray()
+    cols, rows = rimg.shape
+    outarr = np.zeros(shape=rimg.shape)
+    for i in range(rows):
+        for j in range(cols):
+            r=float(rimg[j][i])
+            n=float(nimg[j][i])
+            ndvi=0.
+            d=n+r
+            if d!=0:
+                num=n-r
+                ndvi=num/d
+            outarr[j][i]=scale_ndvi(ndvi, 16)
+    return rimg, outarr
+
+'''
 duckimg=read_image('Data/GI-Duck.jpg')
 nirimg=read_image('Data/imagenir_new.tiff')
 rgbimg=read_image('Data/imagergb.tiff')
@@ -72,4 +107,13 @@ rgbimg=read_image('Data/imagergb.tiff')
 ndvi=generate_ndvi_image(rgbimg, nirimg)
 show_histogram(ndvi, bandwise=False)
 ndvi.show()
-save_image(ndvi,'Data/ndvi.jpg')
+save_image(ndvi,'Outputs/ndvi_8.jpg')
+'''
+nirimg=read_image(r'Data/ikonos-nir.tif')
+rgbimg=read_image(r'Data/ikonos-rgb.tif')
+ndvi=generate_ndvi_image(rgbimg, nirimg)
+save_image(ndvi, 'Outputs/ndvi_8.tif')
+nirimg=read_image(r'Data/ikonos-nir.tif', 16)
+rgbimg=read_image(r'Data/ikonos-rgb.tif', 16)
+rimg, ndvi_arr= generate_ndvi_image_16(rgbimg, nirimg)
+save_img_16(rimg,ndvi_arr,'Outputs/ndvi_16.tif')
